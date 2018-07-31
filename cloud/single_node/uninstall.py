db8731a7b23409288f16983db286de75ebd1ed01
Seongwoon <seanjeong3@gmail.com>
2018-07-31 02:02:35 -0700

    setup.py

---
+++ cloud/single_node/uninstall.py
@@ -0,0 +1,13 @@
+import os
+
+### Read metadata ###
+DataDir = '/datadrive/cassandra'
+DownloadDir = '{0}/cip_setup_tmp'.format(home)
+CassandraDir = '/opt'
+CassandraLinkDir = '{0}/cassandra'.format(home)
+
+### Remove Cassandra ###
+os.system('sudo rm -rf {0}'.format(CassandraLinkDir))
+os.system('sudo rm -rf {0}'.format(CassandraDir))
+os.system('sudo rm -rf {0}'.format(DataDir))
+os.system('sudo rm -rf {0}'.format(DownloadDir))
