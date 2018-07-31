4c0f1cc933e608d9cbb15530f8e954b92773e50d
Seongwoon <seanjeong3@gmail.com>
2018-07-31 14:15:45 -0700

    setup.py

--- cloud/single_node/setup.py
+++ cloud/single_node/setup.py
@@ -1,92 +1,163 @@
 import os
-import socket   
+import socket
+import sys
 
+# Check argument
+if len(sys.argv) != 2:
+	print('You must provide one argument (install or uninstall).')
+	exit(0)
+arg1 = sys.argv[1]
 
 ### Computer-specfic variable ###
 home = os.path.expanduser('~')
 hostname = socket.gethostname()   
 IPAddress = socket.gethostbyname(hostname) 
 
-
 ### Read metadata ###
 DataDir = '/datadrive/cassandra'
 DownloadDir = '{0}/cip_setup_tmp'.format(home)
-CassandraDir = '/opt'
+CassandraDir = '/opt/apache-cassandra-3.7'
 CassandraLinkDir = '{0}/cassandra'.format(home)
 ClusterName = 'Test Cluster'
 Seeds = IPAddress
 ListenAddress = 'localhost'
 
 
-##### Cassandra setup #####
+def UninstallCassandra(): 
+	### Remove old system ###
+	os.system('sudo rm -rf {0}'.format(CassandraLinkDir))
+	os.system('sudo rm -rf {0}'.format(CassandraDir))
+	os.system('sudo rm -rf {0}'.format(DataDir))
+	os.system('sudo rm -rf {0}'.format(DownloadDir))
+	os.system('sudo rm -rf {0}/.cassandra'.format(home))
 
-### Create Cassandra DB data repository folder ###
-os.system('sudo mkdir -p {0}'.format(DataDir))
-os.system('sudo chown -R $USER:$GROUP {0}'.format(DataDir))
-
-### Install pacakages required by Cassandra DB ###
-os.system('sudo apt-get update')
-os.system('sudo apt install openjdk-8-jre-headless')
-os.system('sudo apt install python-pip')
-os.system('pip install cassandra-driver')
-
-### Download Cassandra installer from archive ###
-os.system('mkdir -p {0}'.format(DownloadDir))
-os.system('wget http://archive.apache.org/dist/cassandra/3.7/apache-cassandra-3.7-bin.tar.gz -P {0}'.format(DownloadDir))
-
-### Install (unzip) Cassandra installer ###
-os.system('tar -zxvf {0}/apache-cassandra-3.7-bin.tar.gz -C {0}'.format(DownloadDir))
-os.system('sudo mv {0}/apache-cassandra-3.7 {1}/'.format(DownloadDir, CassandraDir))
-os.system('sudo chown -R $USER:$GROUP {0}/apache-cassandra-3.7/'.format(CassandraDir))
-os.system('ln -s {0}/apache-cassandra-3.7/ {1}'.format(CassandraDir, CassandraLinkDir))
-
-### Update configuration file (cassandra.yaml, cassandra-topology.properties, cassandra-rackdc.properties) ###
-with open('{0}/conf/cassandra.yaml'.format(CassandraLinkDir), 'r') as f :
-	filedata = f.read()
-	f.close()
-
-filedata = filedata.replace('cluster_name: \'Test Cluster\'', 'cluster_name: \'{0}\''.format(ClusterName))
-filedata = filedata.replace('# hints_directory: /var/lib/cassandra/hints', 'hints_directory: {0}'.format(DataDir))
-filedata = filedata.replace('authenticator: AllowAllAuthenticator', 'authenticator: PasswordAuthenticator')
-filedata = filedata.replace('authorizer: AllowAllAuthorizer', 'authorizer: CassandraAuthorizer')
-filedata = filedata.replace('# data_file_directories:', 'data_file_directories:')
-filedata = filedata.replace('#     - /var/lib/cassandra/data', '     - {0}/data'.format(DataDir))
-filedata = filedata.replace('# commitlog_directory: /var/lib/cassandra/commitlog', 'commitlog_directory: {0}/commitlog'.format(DataDir))
-filedata = filedata.replace('# saved_caches_directory: /var/lib/cassandra/saved_caches', 'saved_caches_directory: {0}/saved_caches'.format(DataDir))
-filedata = filedata.replace('- seeds: "127.0.0.1"', '- seeds: "{0}"'.format(Seeds))
-filedata = filedata.replace('listen_address: localhost', 'listen_address: {0}'.format(ListenAddress))
-filedata = filedata.replace('# broadcast_address: 1.2.3.4', 'broadcast_address: {0}'.format(IPAddress))
-filedata = filedata.replace('rpc_address: localhost', 'rpc_address: 0.0.0.0')
-filedata = filedata.replace('# broadcast_rpc_address: 1.2.3.4', 'broadcast_rpc_address: {0}'.format(IPAddress))
-filedata = filedata.replace('# rpc_min_threads: 16', 'rpc_min_threads: 8')
-filedata = filedata.replace('# rpc_max_threads: 2048', 'rpc_max_threads: 256')
-filedata = filedata.replace('read_request_timeout_in_ms: 5000', 'read_request_timeout_in_ms: 20000')
-filedata = filedata.replace('range_request_timeout_in_ms: 10000', 'range_request_timeout_in_ms: 60000')
-filedata = filedata.replace('write_request_timeout_in_ms: 2000', 'write_request_timeout_in_ms: 30000')
-filedata = filedata.replace('counter_write_request_timeout_in_ms: 5000', 'counter_write_request_timeout_in_ms: 30000')
-filedata = filedata.replace('cas_contention_timeout_in_ms: 1000', 'cas_contention_timeout_in_ms: 10000')
-filedata = filedata.replace('truncate_request_timeout_in_ms: 60000', 'truncate_request_timeout_in_ms: 60000')
-filedata = filedata.replace('request_timeout_in_ms: 10000', 'request_timeout_in_ms: 10000')
-filedata = filedata.replace('endpoint_snitch: SimpleSnitch', 'endpoint_snitch: SimpleSnitch')
-filedata = filedata.replace('dynamic_snitch_reset_interval_in_ms: 600000', 'dynamic_snitch_reset_interval_in_ms: 600000')
-filedata = filedata.replace('batch_size_warn_threshold_in_kb: 5', 'batch_size_warn_threshold_in_kb: 5000')
-filedata = filedata.replace('batch_size_fail_threshold_in_kb: 50', 'batch_size_fail_threshold_in_kb: 50000')
-filedata += '\n'
-filedata += 'auto_bootstrap: false'
-
-with open('{0}/conf/cassandra.yaml'.format(CassandraLinkDir), 'w') as f :
-	f.write(filedata)
-	f.close()
+	### Clear env variables
+	with open('{0}/.profile'.format(home), "r+"):
+		lines = f.readlines()
+		f.seek(0)
+		for line in lines:
+			if line not in ['export CQLSH_NO_BUNDLED=true"', 'export PATH=\"/opt/apache-cassandra-3.7/bin:\$PATH\"']
+			f.write(line)
+		f.truncate()
 
 
 
-### Export Cassandra path ###
+def InstallCassandra():
+	##### Cassandra setup #####
 
-### Make session keep alive ###
+	### Create Cassandra DB data repository folder ###
+	os.system('sudo mkdir -p {0}'.format(DataDir))
+	os.system('sudo chown -R $USER:$GROUP {0}'.format(DataDir))
 
-### Enable auth-auth ###
+	### Install pacakages required by Cassandra DB ###
+	os.system('sudo apt-get update')
+	os.system('sudo apt install openjdk-8-jre-headless')
+	os.system('sudo apt install python-pip')
+	os.system('pip install cassandra-driver')
 
-### Apply data schema ###
+	### Download Cassandra installer from archive ###
+	os.system('mkdir -p {0}'.format(DownloadDir))
+	os.system('wget http://archive.apache.org/dist/cassandra/3.7/apache-cassandra-3.7-bin.tar.gz -P {0}'.format(DownloadDir))
+
+	### Install (unzip) Cassandra installer ###
+	os.system('tar -zxvf {0}/apache-cassandra-3.7-bin.tar.gz -C {0}'.format(DownloadDir))
+	os.system('sudo mv {0}/apache-cassandra-3.7/ {1}/'.format(DownloadDir, CassandraDir))
+	os.system('sudo chown -R $USER:$GROUP {0}'.format(CassandraDir))
+	os.system('ln -s {0} {1}'.format(CassandraDir, CassandraLinkDir))
+
+	### Update configuration file (cassandra.yaml, cassandra-topology.properties, cassandra-rackdc.properties) ###
+	with open('{0}/conf/cassandra.yaml'.format(CassandraLinkDir), 'r') as f :
+		filedata = f.read()
+
+	filedata = filedata.replace('cluster_name: \'Test Cluster\'', 'cluster_name: \'{0}\''.format(ClusterName))
+	filedata = filedata.replace('# hints_directory: /var/lib/cassandra/hints', 'hints_directory: {0}'.format(DataDir))
+	filedata = filedata.replace('authenticator: AllowAllAuthenticator', 'authenticator: PasswordAuthenticator')
+	filedata = filedata.replace('authorizer: AllowAllAuthorizer', 'authorizer: CassandraAuthorizer')
+	filedata = filedata.replace('# data_file_directories:', 'data_file_directories:')
+	filedata = filedata.replace('#     - /var/lib/cassandra/data', '     - {0}/data'.format(DataDir))
+	filedata = filedata.replace('# commitlog_directory: /var/lib/cassandra/commitlog', 'commitlog_directory: {0}/commitlog'.format(DataDir))
+	filedata = filedata.replace('# saved_caches_directory: /var/lib/cassandra/saved_caches', 'saved_caches_directory: {0}/saved_caches'.format(DataDir))
+	filedata = filedata.replace('- seeds: "127.0.0.1"', '- seeds: "{0}"'.format(Seeds))
+	filedata = filedata.replace('listen_address: localhost', 'listen_address: {0}'.format(ListenAddress))
+	filedata = filedata.replace('# broadcast_address: 1.2.3.4', 'broadcast_address: {0}'.format(IPAddress))
+	filedata = filedata.replace('rpc_address: localhost', 'rpc_address: 0.0.0.0')
+	filedata = filedata.replace('# broadcast_rpc_address: 1.2.3.4', 'broadcast_rpc_address: {0}'.format(IPAddress))
+	filedata = filedata.replace('# rpc_min_threads: 16', 'rpc_min_threads: 8')
+	filedata = filedata.replace('# rpc_max_threads: 2048', 'rpc_max_threads: 256')
+	filedata = filedata.replace('read_request_timeout_in_ms: 5000', 'read_request_timeout_in_ms: 20000')
+	filedata = filedata.replace('range_request_timeout_in_ms: 10000', 'range_request_timeout_in_ms: 60000')
+	filedata = filedata.replace('write_request_timeout_in_ms: 2000', 'write_request_timeout_in_ms: 30000')
+	filedata = filedata.replace('counter_write_request_timeout_in_ms: 5000', 'counter_write_request_timeout_in_ms: 30000')
+	filedata = filedata.replace('cas_contention_timeout_in_ms: 1000', 'cas_contention_timeout_in_ms: 10000')
+	filedata = filedata.replace('truncate_request_timeout_in_ms: 60000', 'truncate_request_timeout_in_ms: 60000')
+	filedata = filedata.replace('request_timeout_in_ms: 10000', 'request_timeout_in_ms: 10000')
+	filedata = filedata.replace('endpoint_snitch: SimpleSnitch', 'endpoint_snitch: SimpleSnitch')
+	filedata = filedata.replace('dynamic_snitch_reset_interval_in_ms: 600000', 'dynamic_snitch_reset_interval_in_ms: 600000')
+	filedata = filedata.replace('batch_size_warn_threshold_in_kb: 5', 'batch_size_warn_threshold_in_kb: 5000')
+	filedata = filedata.replace('batch_size_fail_threshold_in_kb: 50', 'batch_size_fail_threshold_in_kb: 50000')
+	filedata += '\n'
+	filedata += 'auto_bootstrap: false'
+
+	with open('{0}/conf/cassandra.yaml'.format(CassandraLinkDir), 'w') as f :
+		f.write(filedata)
+
+	### Export Cassandra path ###
+	os.system('echo "export CQLSH_NO_BUNDLED=true" >> {0}/.profile'.format(home))
+	os.system('echo "export PATH=\"/opt/apache-cassandra-3.7/bin:\$PATH\"" >> {0}/.profile'.format(home))
+	os.system('source {0}/.profile'.format(home))
+
+	### Make session keep alive ###
+	os.system('sudo sysctl -w net.ipv4.tcp_keepalive_time=60 net.ipv4.tcp_keepalive_probes=3 net.ipv4.tcp_keepalive_intvl=10')
+
+	### Enable auth-auth ###
+	os.system('mkdir {0}/.cassandra'.format(home))
+	os.system('touch {0}/.cassandra/cqlshrc'.format(home))
+	os.system('echo "[authentication]" >> {0}/.cassandra/cqlshrc'.format(home))
+	os.system('echo "username = cassandra >> {0}/.cassandra/cqlshrc'.format(home))
+	os.system('echo "password = cassandra" >> {0}/.cassandra/cqlshrc'.format(home))
+
+	### Launch Cassandra -f ###
+
+	### Wait until Cassandra runs ###
+
+	### Apply data schema ###
 
 
-##### Node.js Setup #####
+	
+
+
+if arg1 == 'install':
+	while True:
+		ans = raw_input('This action will remove data in directories {0}, {1}, {2} and {3} and install a new platform. Do you want to proceed? (yes/no): '.format(DataDir, DownloadDir, CassandraDir, CassandraLinkDir))
+		if ans == 'yes':
+			break
+		elif ans == 'no':
+			print('Installation has been canceled.')
+			exit(0)
+		else:
+			print('Please answer in "yes" or "no".')
+	UninstallCassandra()
+	InstallCassandra()
+	
+	##### Node.js Setup #####
+
+
+elif arg1 == 'uninstall':
+	while True:
+		ans = raw_input('This action will remove data in directories {0}, {1}, {2} and {3}. Do you want to proceed? (yes/no): '.format(DataDir, DownloadDir, CassandraDir, CassandraLinkDir))
+		if ans == 'yes':
+			break
+		elif ans == 'no':
+			print('Uninstall has been canceled.')
+			exit(0)
+		else:
+			print('Please answer in "yes" or "no".')
+	UninstallCassandra()
+
+
+
+
+else:
+	print('Invalid argument {0}.'.format(arg1))
+
+	
---
+++ cloud/single_node/test.py
@@ -0,0 +1,10 @@
+import os
+import socket
+import sys
+# Check argument
+if len(sys.argv) != 2:
+	print('You must provide one argument (install or uninstall).')
+	exit(0)
+arg1 = sys.argv[1]
+
+print "hi"
