83a0d12eb29cd7e9572e2eca745cae3af3ab5c1f
Seongwoon <seanjeong3@gmail.com>
2018-07-30 18:52:13 -0700

    update setup.py

---
+++ cloud/single_node/setup.py
@@ -0,0 +1,31 @@
+import os
+
+### Read metadata ###
+DataDir = '/datadrive/cassandra'
+DownloadDir = '~/cip_setup_tmp'
+CassandraDir = '/opt'
+CassandraLinkDir = '~/cassandra'
+
+##### Cassandra setup #####
+
+### Create Cassandra DB data repository folder ###
+os.system('sudo mkdir {0}'.format(DataDir))
+os.system('sudo chown -R $USER:$GROUP {0}'.format(DataDir))
+
+### Install pacakages required by Cassandra DB ###
+os.system('sudo apt-get update')
+os.system('sudo apt install openjdk-8-jre-headless')
+os.system('sudo apt install python-pip')
+os.system('pip install cassandra-driver')
+
+### Download Cassandra installer from archive ###
+os.system('mkdir {0}'.format(DownloadDir))
+os.system('wget http://archive.apache.org/dist/cassandra/3.7/apache-cassandra-3.7-bin.tar.gz -P {0}'.format(DownloadDir))
+
+### Install (unzip) Cassandra ###
+os.system('tar -zxvf {0}/apache-cassandra-3.7-bin.tar.gz'.format(DownloadDir))
+os.system('sudo mv {0}/apache-cassandra-3.7 {1}'.format(DownloadDir, CassandraDir))
+os.system('sudo chown -R $USER:$GROUP {0}/apache-cassandra-3.7/'.format(CassandraDir))
+os.system('ln -s {0}/apache-cassandra-3.7/ {1}'.format(CassandraLinkDir))
+
+
