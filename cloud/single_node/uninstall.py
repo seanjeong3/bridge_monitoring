a02dc1ef4c0eeb34fad90ce97c04db83f6cc8b39
Seongwoon <seanjeong3@gmail.com>
2018-07-31 02:19:57 -0700

    setup.py

--- cloud/single_node/uninstall.py
+++ cloud/single_node/uninstall.py
@@ -12,6 +12,6 @@ CassandraLinkDir = '{0}/cassandra'.format(home)
 
 ### Remove Cassandra ###
 os.system('sudo rm -rf {0}'.format(CassandraLinkDir))
-os.system('sudo rm -rf {0}'.format(CassandraDir))
+os.system('sudo rm -rf {0}/apache-cassandra-3.7'.format(CassandraDir))
 os.system('sudo rm -rf {0}'.format(DataDir))
 os.system('sudo rm -rf {0}'.format(DownloadDir))
